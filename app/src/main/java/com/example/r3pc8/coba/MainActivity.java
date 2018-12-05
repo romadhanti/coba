f373556f2b3c8c7618b22914504de62f93394dee
romadhanti <45616551+romadhanti@users.noreply.github.com>
2018-12-05 12:31:14 +0700

    tambah textview

--- app/src/main/java/com/example/r3pc8/coba/MainActivity.java
+++ app/src/main/java/com/example/r3pc8/coba/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.r3pc8.coba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tes1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
